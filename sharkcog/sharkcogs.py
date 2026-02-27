80ac8e24a1c4b0da9bb1fc851dd0eb70646ea1ee
zigz <a>
2026-02-27 01:54:10 -0800

    Fix folder structure

--- sharkcog/sharkcogs.py
+++ sharkcog/sharkcogs.py
@@ -22,7 +22,7 @@ class SharkCog(commands.Cog):
         )
 
 
-    async def dice(self, ctx):
+    async def sharkdice(self, ctx):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("Roll Test!")
