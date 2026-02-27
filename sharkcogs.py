c5eb54e105c896e7a9fd0808295fdac694ddf113
zigz <a>
2026-02-27 01:47:47 -0800

    Test cogs

---
+++ sharkcogs.py
@@ -0,0 +1,32 @@
+from typing import Literal
+
+import discord
+from redbot.core import commands
+from redbot.core.bot import Red
+from redbot.core.config import Config
+
+RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
+
+
+class SharkCog(commands.Cog):
+    """
+    Baby shark doo do dooo
+    """
+
+    def __init__(self, bot: Red) -> None:
+        self.bot = bot
+        self.config = Config.get_conf(
+            self,
+            identifier=28572174,
+            force_registration=True,
+        )
+
+
+    async def dice(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("Roll Test!")
+
+    async def red_delete_data_for_user(self, *, requester: RequestType, user_id: int) -> None:
+        # TODO: Replace this with the proper end user data removal handling.
+        super().red_delete_data_for_user(requester=requester, user_id=user_id)
